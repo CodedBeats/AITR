c993828c76dfa4cc91c1a554df49f3505a3f7dc8
CodedBeats <luca.haar@icloud.com>
2024-11-10 23:35:37 +1100

    project init, db connection, simple grid data -- initilised AITR project in VS, setup DB connection with MS SQL Server Manager, displaying simple grid with testing tables from DB

---
+++ AITR/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AITR.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbTitle" runat="server" Text="Welcome to AITR" Font-Size="60" Font-Bold="True" ForeColor="#FF0066" />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <asp:GridView ID="gvUser" runat="server"
+                OnRowDataBound="gvUser_RowDataBound">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AITR/ExceptionHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace AITR
+{
+    public class ExceptionHandler
+    {
+        private Label _label;
+
+        public ExceptionHandler(Label label)
+        {
+            _label = label;
+        }
+
+        public void HandleExceptions(Exception ex)
+        {
+            if (ex is InvalidCastException)
+            {
+                HandleInvalidCastException(ex as InvalidCastException);
+            }
+            else if (ex is SqlException)
+            {
+                HandleSqlException(ex as SqlException);
+            }
+            else if (ex is InvalidOperationException)
+            {
+                HandleInvalidOperationException(ex as InvalidOperationException);
+            }
+            else if (ex is ConfigurationErrorsException)
+            {
+                HandleConfigurationErrorsException(ex as ConfigurationErrorsException);
+            }
+            else
+            {
+                HandleGenericException(ex);
+            }
+        }
+
+        private void HandleInvalidCastException(Exception ex)
+        {
+            _label.Text = "Data Type mismatch. \nError detail: " + ex.Message;
+        }
+        private void HandleSqlException(SqlException ex)
+        {
+            _label.Text = "Internal Error, please contact database admin. \nError detail: " + ex.Message;
+        }
+        private void HandleInvalidOperationException(InvalidOperationException ex)
+        {
+            _label.Text = "Internal Error, please contact system admin. \nError detail: " + ex.Message;
+        }
+        private void HandleConfigurationErrorsException(ConfigurationErrorsException ex)
+        {
+            _label.Text = "Internal Error, please contact system admin. \nError detail: " + ex.Message;
+        }
+        private void HandleGenericException(Exception ex)
+        {
+            _label.Text = "Internal Error, please contact system admin. \nError detail: " + ex.Message;
+        }
+    }
+}
